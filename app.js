0b7c4645fda05d8e8fbbab8d386049fc83795840
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-24 14:31:06 -0500

    add-test-post-route

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const PORT = 3000;
 
 //Routes
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server');
+  res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+});
+app.get('/', (req, res) => {
+  res.send('you can post to this endpoint');
 });
 
 app.listen(PORT, () => {
