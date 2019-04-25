13f4efc1509c095f12e2b8ef1b57e7b783d1aa8f
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-25 16:05:17 -0500

    get-tour/add-route-get-tour

--- app.js
+++ app.js
@@ -32,6 +32,19 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
