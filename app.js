fe2bb86e9ed0c63e0f9cf3db0c7b7b2e812b3be0
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-27 16:53:30 -0500

    test-app.js-middleware

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from the middleware    ');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const PORT = 3000;
 
@@ -13,9 +22,13 @@ const tours = JSON.parse(
 //Routes
 
 const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours }
+  });
 };
 const getTour = (req, res) => {
   console.log(req.params);
