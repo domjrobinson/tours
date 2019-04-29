11fef542f07225f48903074515db7d1b4763647c
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-29 18:06:24 -0500

    get-tour/create-middleware-to-check-id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,15 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -17,10 +26,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
 
   const tour = tours.find(el => el.id === id);
 
@@ -43,18 +48,10 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
-
   res
     .status(200)
     .json({ status: 'success', data: { tour: 'Updated tour here' } });
 };
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
-
   res.status(204).json({ status: 'success', data: null });
 };
