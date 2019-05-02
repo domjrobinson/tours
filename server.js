0bcc4bd29f14272eb96447d1fbee220bee055d39
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-01 19:07:11 -0500

    fix-linting-errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app.js');
+
+dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
