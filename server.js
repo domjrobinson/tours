882040499c5b50cfd721fcb0424e7094da430a34
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-02 10:43:29 -0500

    tour-schema/add-tour-schema-model

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ mongoose
     console.log('DB connection successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+    unique: true
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'a tour must have a price'] }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
