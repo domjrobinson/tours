eeabf57ac75cc1b3d3896e0b2e7f4574927cecae
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-03 11:12:07 -0500

    tour-model/move-model-into-module

--- server.js
+++ server.js
@@ -14,21 +14,8 @@ mongoose
     useFindAndModify: false
   })
   .then(con => {
-    console.log(con.connections);
     console.log('DB connection successful');
   });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a price'],
-    unique: true
-  },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'a tour must have a price'] }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
