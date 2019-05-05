18926987ee33c3133dfaf165edae8943fffa9fe6
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-04 22:35:25 -0500

    mongodb/setup-connection-to-database

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app.js');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
