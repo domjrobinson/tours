17aa486a8458361609f87d15ccd40404ecffde9a
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-09 19:31:00 -0500

    document middleare testing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       unique: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -60,6 +62,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationweeks').get(function() {
   return this.duration / 7;
 });
+
+// Document Middleware / run before .save and .create
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
