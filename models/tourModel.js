c6616e7634491298fcbfc01ae51d72516391ae81
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-12 14:38:23 -0500

    tour-model/add-validator.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,7 +10,8 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       unique: true,
       maxLength: [40, 'A tour name must have less or equal than 40 characters'],
-      minLength: [10, 'A tour must have more or equal than 10 charachters']
+      minLength: [10, 'A tour must have more or equal than 10 charachters'],
+      validate: validator.isAlpha
     },
     slug: String,
     duration: {
@@ -42,7 +44,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'a tour must have a price']
     },
-    discount: { type: Number },
+    discount: {
+      type: Number,
+      validate: {
+        message: 'Discount price ({VALUE}) should be below regular price',
+        validator: function(val) {
+          // `this` points to the current document
+          return val < this.price;
+        }
+      }
+    },
     summary: {
       type: String,
       required: [true, 'A tour must have a description'],
