2e74879334725a69b21c69eeba151175c248913e
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-14 15:21:08 -0500

    refactor-global-error-handling-middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -35,13 +36,6 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl}`));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
