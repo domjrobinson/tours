610489326a117d562c004021c8fcda338b80cf39
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-16 16:38:38 -0500

    send-error-prod/add-operation-error-handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,10 +7,14 @@ const sendErrorDev = (err, res) => {
   });
 };
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    res.status(500).json({ status: 'error', message: 'Something went wrong' });
+  }
 };
 
 module.exports = (err, req, res, next) => {
