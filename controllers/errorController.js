d3bdaa46b34eab1ff3a082cf4921ad0fb737f2fc
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-17 16:49:42 -0500

    app-error/handle-cast-error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,10 @@
 const AppError = require('./../utils/AppError');
 
-const handleCastErrorDB = () => {};
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
