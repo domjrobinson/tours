d6ab0600a9b31f91e54d3db2e6d158437324227f
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-17 16:55:13 -0500

    send-error-prod-bug-fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,7 +21,7 @@ const sendErrorProd = (err, res) => {
       message: err.message
     });
   } else {
-    console.err('ERROR', err);
+    console.error('ERROR', err);
     res.status(500).json({ status: 'error', message: 'Something went wrong' });
   }
 };
