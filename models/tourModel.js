6c4f08892f49bae19ebc234b7136579de930c7fd
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-18 21:32:38 -0500

    comment-out-validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,8 +10,8 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       unique: true,
       maxLength: [40, 'A tour name must have less or equal than 40 characters'],
-      minLength: [10, 'A tour must have more or equal than 10 charachters'],
-      validate: validator.isAlpha
+      minLength: [10, 'A tour must have more or equal than 10 charachters']
+      // validate: validator.isAlpha
     },
     slug: String,
     duration: {
