8ba899d63b900d5d97f70f29dce8cbfd92d71d8b
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-19 22:15:57 -0500

    handle-uncaught-exceptions

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app.js');
 
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION, Shutting down');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
