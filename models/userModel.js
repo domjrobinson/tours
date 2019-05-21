13bd1552aa8197dafffc5fa597315b2b10d60d6a
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-20 22:44:01 -0500

    user-model/add-user-model-schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
+const validator = require('validate');
 const slugify = require('slugify');
 
-const userSchema = new mongoose.Schema();
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: [8, 'Password must be 8 or more charcters']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
 
 const User = mongoose.model('Tour', userSchema);
 
