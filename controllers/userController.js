09788d09520d1e168cc5bb86fbf93e20f9a73204
Dominique Robinson <domrobinson.dj@gmail.com>
2019-05-25 00:11:48 -0500

    get-all-users/add-get-all-users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defind' });
-};
+const catchAsync = require('./../utils/catchAsync.js');
+const User = require('./../models/userModel');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: { users }
+  });
+});
 exports.getUser = (req, res) => {
   res
     .status(500)
