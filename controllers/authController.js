cc685d468e5055fbf094948571382b61ce116894
Dominique Robinson <domrobinson.dj@gmail.com>
2019-07-05 14:55:11 -0500

    create-send-token-addupdate-password

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,16 @@ const signToken = id =>
     expiresIn: process.env.JWT_EXPIRES_IN
   });
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: { user }
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
 
@@ -20,14 +30,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password,
     passwordConfirm
   });
-
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: { user: newUser }
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -45,8 +48,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) send token
-  const token = signToken(user._id);
-  res.status(200).json({ status: 'success', token });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -154,11 +156,20 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetExpires = undefined;
   await user.save();
   // log the user in send jwt
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
+});
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: { user: user }
-  });
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong', 401));
+  }
+  console.log('user', user);
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  createSendToken(user, 201, res);
 });
