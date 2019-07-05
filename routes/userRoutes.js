068b424b938590aff198870de3e87bc678669b39
Dominique Robinson <domrobinson.dj@gmail.com>
2019-07-05 15:22:08 -0500

    update-user-routes-with-update-me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,8 @@ router.post(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
