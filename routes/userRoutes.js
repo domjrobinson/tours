bda47e4f934d291c01f453625d3c91210101e2a7
Dominique Robinson <domrobinson.dj@gmail.com>
2019-07-19 15:29:36 -0500

    update-user-routes-delete-me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,7 @@ router.post(
 );
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')
