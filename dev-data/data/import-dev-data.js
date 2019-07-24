73dd8a6b6de05078715d72d29c73220cd921c7ad
Dominique Robinson <domrobinson.dj@gmail.com>
2019-07-24 00:28:24 -0500

    update-tour-model-with-geo-data-schema-types

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -22,9 +22,7 @@ mongoose
 
 //  read json filename
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
