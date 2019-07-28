dd008771a2fd33abf72ee990465a3002d21997e2
Dominique Robinson <domrobinson.dj@gmail.com>
2019-07-27 22:54:07 -0500

    mount

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authController = require('./../controllers/authController');
 const reviewsController = require('./../controllers/reviewsController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
