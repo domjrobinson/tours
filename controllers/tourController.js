8f76b0600d12bf312c6531be55e23f83a8777669
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-04 11:41:19 -0500

    create-tour/add-create-tour-and-persist-to-db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,19 +12,25 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   console.log(req.params);
-  const id = req.params.id * 1;
+  // const id = req.params.id * 1;
 
   // const tour = tours.find(el => el.id === id);
 
   // res.status(200).json({ status: 'success', data: { tour } });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
   // console.log('req.body', req.body);
-  res.status(201).json({
-    status: 'success'
-    // data: { tour: newTour }
-  });
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour }
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: 'Invalid data set' });
+  }
 };
 
 exports.updateTour = (req, res) => {
