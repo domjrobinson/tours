f5f20b0d81a8bfeaa9c24ebabb554767c20ddb60
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-04 12:29:17 -0500

    delete-tour/add-delete-tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,6 +55,13 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
+exports.deleteTour = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Tour.findByIdAndDelete(id);
+
+    res.status(204).json({ status: 'success', data: null });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: 'Invalid data set' });
+  }
 };
