038b9d609fcf733fcbd33bbd261a986fa2d7b07e
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-05 00:39:00 -0500

    refactor-with-factory-handlers

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -2,9 +2,6 @@ const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewsModel');
 const factory = require('./handlerFactory');
 
-exports.updateReview = factory.updateOne(Review);
-exports.deleteReview = factory.deleteOne(Review);
-
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourID) filter = { tour: req.params.tourId };
@@ -20,17 +17,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // * Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.toursID;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview
-    }
-  });
-});
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
+exports.createReview = factory.createOne(Review);
