82e037461ca3be4b3065e5f90a425b76576f131e
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-06 14:19:41 -0500

    update-routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewsRouter = require('./../controllers/errorController.js');
+const reviewRouter = require('./../routes/reviewsRoutes');
 
 const router = express.Router();
 
-router.use('/:tourId/reviews', reviewsRouter);
+router.use('/:tourId/reviews', reviewRouter);
 
-// router.param('id', tourController.checkID);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router.route('/tour-stats').get(tourController.getTourStats);
 
 router
   .route('/monthly-plan/:year')
   .get(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide', 'guides'),
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
     tourController.getMonthlyPlan
   );
 
 router
-  .route('/top-5-cheap')
-  .get(tourController.aliasTopTours, tourController.getAllTours);
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
 router
   .route('/')
