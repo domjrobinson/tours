416c1e2c420e783992b4bd0050ce6981444bf0cd
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-06 22:45:18 -0500

    reviews-add-calculate-review-statistics

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -46,6 +47,32 @@ reviewSchema.pre('/^find/', function(next) {
   });
   next();
 });
+
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $num: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+reviewSchema.post('save', function() {
+  // this points to current review
+  this.constructor.Review.calcAverageRating(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
