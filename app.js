bcc715e55142ce69f83f81a4c0a83d9a57486f64
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-07 23:11:07 -0500

    typo

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The orrest Hiker',
+    tour: 'The Forrest Hiker',
     user: 'Dom'
   });
 });
