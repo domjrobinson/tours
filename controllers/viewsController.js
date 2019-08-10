30acc325e545bbf5ec7d7c6983f5fdb881762ddf
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-09 23:51:28 -0500

    getTours/add-get-tours-and-send-to-client

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.viewOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.viewOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
