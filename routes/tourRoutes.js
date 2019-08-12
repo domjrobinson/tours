f52bc9234ad2830b1d1ff56669d632036f4742bf
Dominique Robinson <domrobinson.dj@gmail.com>
2019-08-12 14:31:09 -0500

    add-authorization-to-routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,14 @@ router.use('/:tourId/reviews', reviewsRouter);
 
 // router.param('id', tourController.checkID);
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guides'),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/top-5-cheap')
@@ -17,13 +24,21 @@ router
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
